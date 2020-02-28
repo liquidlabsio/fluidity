6ea7f0920223e7b3d8b42aac63e226b0e75a1ff9
Neil Avery <neil.avery@liquidlabs.com>
2020-02-28 21:40:30 +0000

    - Minor tweaks to AWS usage
    Signed-off-by: Neil Avery <neil.avery@liquidlabs.com>

--- services/src/test/java/io/precognito/services/NativeSearchResourceTest.java
+++ services/src/test/java/io/precognito/services/NativeSearchResourceTest.java
@@ -1,8 +1,11 @@
 package io.precognito.services;
 
+import io.quarkus.test.junit.QuarkusTest;
+
 // TODO: revisit -
 //  error  Unable to automatically find native image, please set the native.image.path to the native executable you wish to test
 //@NativeImageTest
+@QuarkusTest
 class NativeSearchResourceTest extends SearchResourceTest {
 
 }
