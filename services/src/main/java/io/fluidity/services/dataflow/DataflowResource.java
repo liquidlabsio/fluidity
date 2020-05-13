01216cb0e48383d34dd93213eb644f7e7448848f
Neil Avery <neil.avery@liquidlabs.com>
2020-05-13 09:03:53 +0100

    fixing copyright templating
    Signed-off-by: Neil Avery <neil.avery@liquidlabs.com>

--- services/src/main/java/io/fluidity/services/dataflow/DataflowResource.java
+++ services/src/main/java/io/fluidity/services/dataflow/DataflowResource.java
@@ -1,21 +1,12 @@
 /*
+ *  Copyright (c) 2020. Liquidlabs Ltd <info@liquidlabs.com>
  *
+ *  This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  *
- * *
- *  * Copyright (c) 2020. Liquidlabs Ltd <info@liquidlabs.com>
- *  *
- *  * This program is free software: you can redistribute it and/or modify
- *  * it under the terms of the GNU Affero General Public License as
- *  * published by the Free Software Foundation, either version 3 of the
- *  * License, or (at your option) any later version.
- *  *
- *  * This program is distributed in the hope that it will be useful,
- *  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  * GNU Affero General Public License for more details.
- *  *
- *  * You should have received a copy of the GNU Affero General Public License
- *  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 
@@ -50,7 +41,7 @@ import java.nio.charset.StandardCharsets;
 import static io.fluidity.dataflow.Model.CORR_PREFIX;
 
 /**
- * API to build, maintain and access dataflow models
+ * API to build, maintain, and access dataflow models
  */
 @Path("/dataflow")
 @Consumes(MediaType.APPLICATION_JSON)
