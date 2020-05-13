021b2ca87b9e2c0de156a613c339ae2120d804e1
Neil Avery <neil.avery@liquidlabs.com>
2020-05-13 09:30:14 +0100

    fixing test
    Signed-off-by: Neil Avery <neil.avery@liquidlabs.com>

--- services/src/main/java/io/fluidity/services/dataflow/DataflowResource.java
+++ services/src/main/java/io/fluidity/services/dataflow/DataflowResource.java
@@ -3,8 +3,7 @@
  *
  *  This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Affero General Public License for more details.
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
  *
  *  You should have received a copy of the GNU Affero General Public License  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
@@ -108,7 +107,13 @@ public class DataflowResource implements DataflowService {
         };
         String userSession = runner.run(search, sessionId);
         log.info(LogHelper.format(sessionId, "dataflow", "submit", "Starting:" + search));
-        return userSession;
+
+        try {
+            return new ObjectMapper().writeValueAsString(userSession);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return "{ \"msg\": \"failed\"}";
     }
 
     /**
--- services/src/test/java/io/fluidity/services/dataflow/DataflowResourceTest.java
+++ services/src/test/java/io/fluidity/services/dataflow/DataflowResourceTest.java
@@ -1,3 +1,14 @@
+/*
+ *  Copyright (c) 2020. Liquidlabs Ltd <info@liquidlabs.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package io.fluidity.services.dataflow;
 
 import io.fluidity.search.Search;
@@ -60,10 +71,9 @@ class DataflowResourceTest {
                 .post("/dataflow/submit/{tenant}/{serviceAddress}/{modelName}")
                 .then()
                 .statusCode(200).extract();
-        String[] as = response.body().as(String[].class);
+        String as = response.body().as(String.class);
 
-
-        System.out.println("Got:" + response);
+        System.out.println("Got:" + as);
 
     }
 }
