102287be5ca77df7df6a2222914cffb329be6fdd
Neil Avery <neil.avery@liquidlabs.com>
2020-05-13 08:59:12 +0100

    fixing Codacy issues
    Signed-off-by: Neil Avery <neil.avery@liquidlabs.com>

--- services/src/main/java/io/fluidity/services/dataflow/DataflowResource.java
+++ services/src/main/java/io/fluidity/services/dataflow/DataflowResource.java
@@ -1,3 +1,24 @@
+/*
+ *
+ *
+ * *
+ *  * Copyright (c) 2020. Liquidlabs Ltd <info@liquidlabs.com>
+ *  *
+ *  * This program is free software: you can redistribute it and/or modify
+ *  * it under the terms of the GNU Affero General Public License as
+ *  * published by the Free Software Foundation, either version 3 of the
+ *  * License, or (at your option) any later version.
+ *  *
+ *  * This program is distributed in the hope that it will be useful,
+ *  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  * GNU Affero General Public License for more details.
+ *  *
+ *  * You should have received a copy of the GNU Affero General Public License
+ *  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package io.fluidity.services.dataflow;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -29,7 +50,7 @@ import java.nio.charset.StandardCharsets;
 import static io.fluidity.dataflow.Model.CORR_PREFIX;
 
 /**
- * Builds a dataflow by fanning out to Lambdas, returning status on progress
+ * API to build, maintain and access dataflow models
  */
 @Path("/dataflow")
 @Consumes(MediaType.APPLICATION_JSON)
