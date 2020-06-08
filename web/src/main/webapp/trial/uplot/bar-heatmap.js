e808f526622245529830b2a082aa3a7530915dd4
bluemonk3y <neil.avery@gmail.com>
2020-06-08 19:09:59 +0100

    click handler working
    Signed-off-by: bluemonk3y <neil.avery@gmail.com>

--- web/src/main/webapp/trial/uplot/bar-heatmap.js
+++ web/src/main/webapp/trial/uplot/bar-heatmap.js
@@ -59,6 +59,113 @@ function columnHighlightPlugin({ className, style = {backgroundColor: "rgba(51,2
         }
     };
 }
+function tooltipsPlugin(opts) {
+    function init(u, opts, data) {
+        let plot = u.root.querySelector(".over");
+
+        let ttc = u.cursortt = document.createElement("div");
+        ttc.className = "tooltip";
+        ttc.textContent = "(x,y)";
+        ttc.style.pointerEvents = "none";
+        ttc.style.position = "absolute";
+        ttc.style.background = "rgba(0,0,255,0.1)";
+        plot.appendChild(ttc);
+
+        u.seriestt = opts.series.map((s, i) => {
+            if (i == 0) return;
+
+            let tt = document.createElement("div");
+            tt.className = "tooltip";
+            tt.textContent = "Tooltip!";
+            tt.style.pointerEvents = "none";
+            tt.style.position = "absolute";
+            tt.style.background = "rgba(0,0,0,0.1)";
+            tt.style.color = s.color;
+            tt.style.display = s.show ? null : "none";
+            plot.appendChild(tt);
+            return tt;
+        });
+
+        function hideTips() {
+            ttc.style.display = "none";
+            u.seriestt.forEach((tt, i) => {
+                if (i == 0) return;
+                tt.style.display = "none";
+            });
+        }
+
+        function showTips() {
+            ttc.style.display = null;
+            u.seriestt.forEach((tt, i) => {
+                if (i == 0) return;
+
+                let s = u.series[i];
+                tt.style.display = s.show ? null : "none";
+            });
+        }
+
+        plot.addEventListener("mouseleave", () => {
+            if (!u.cursor.locked) {
+                hideTips();
+            }
+        });
+
+        plot.addEventListener("mouseenter", () => {
+            showTips();
+        });
+
+        hideTips();
+    }
+
+    function setCursor(u) {
+        const {left, top, idx} = u.cursor;
+
+        let tt = u.seriestt[1]
+        let s = u.series[1];
+
+        let xVal = u.data[0][idx];
+        let yVal = u.data[1][idx];
+        if (yVal == null) return;
+        let zVal = u.data[2][idx];
+
+        tt.textContent = "-(" + yVal + " hits:" + zVal + ")-";
+
+        tt.style.left = Math.round(u.valToPos(xVal, 'x')) + "px";
+        tt.style.top = Math.round(u.valToPos(yVal, s.scale)) + "px";
+
+
+        u.cursortt.style.left = left + "px";
+        u.cursortt.style.top = top + "px";
+
+        // OLD - basic display
+        u.cursortt.textContent = "( " + Math.round(top, "y") + ")";
+        // scan the series data for a hit
+        let liveYValue = Math.round(u.posToVal(top, "y"));
+
+        let foundIndex = yVal.indexOf(liveYValue);
+        if (foundIndex >= 0){
+            u.cursortt.textContent = "Latency:" +liveYValue + " Hits: " + zVal[foundIndex];
+        }
+    }
+
+    return {
+        hooks: {
+            init,
+            setCursor,
+            setScale: [
+                (u, key) => {
+                    console.log('setScale', key);
+                }
+            ],
+            setSeries: [
+                (u, idx) => {
+                    console.log('setSeries', idx);
+                }
+            ],
+        },
+    };
+}
+
 
 
 function ladderRenderPlugin({ gap = 2, shadowColor = "#000000", bodyMaxWidth = 50 } = {}) {
@@ -158,19 +265,14 @@ const opts = {
     plugins: [
        columnHighlightPlugin(),
        ladderRenderPlugin(),
+       tooltipsPlugin(),
     ],
       series: [
             {},
             {
                 paths: () => null,
               points: {show: false},
-            },
-              {
-                paths: () => null,
-              points: {show: false},
-            },
-//            { }
-
+            }
         ],
     scales: {
         y: {
@@ -199,3 +301,17 @@ const opts = {
 };
 
 let u = new uPlot(opts, data, document.body);
+
+u.root.querySelector(".over").addEventListener('click', function(e1, e2) {
+
+    const {left, top, idx} = u.cursor;
+    let xVal = u.data[0][idx];
+    let yVal = u.data[1][idx];
+    (u.valToPos(xVal, 'x'))
+    let liveXValue = Math.round(u.posToVal(top, "x").toFixed(0));
+    let liveYValue = Math.round(u.posToVal(top, "y"));
+    console.log("yay:" + u.cursor.idx)
+
+
+
+})
